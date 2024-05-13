3c88c6fa5f1e7f9f69b4c899f76d6cc85196cb2f
Sarah Batagioti <sarah.montuanibt@gmail.com>
2024-05-13 10:40:26 -0300

    Configuração inicial banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+# Configuração do MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'tasks'
+mysql = MySQL(app)
+
+
+@app.route('/add', methods=['POST'])
+def add_contato():
+    if request.method == 'POST':
+        ir_de= request.form['ir_de']
+        ir_para = request.form['ir_para']
+        data_ida = request.form['data_ida']
+        data_volta = request.form['data_volta']
+        descrição = request.form['descrição']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contato (ir_de, ir_para, data_ida, data_volta, descrição) VALUES (%s, %s)", (ir_de, ir_para, data_ida, data_volta, descrição))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
---
+++ script.sql
@@ -0,0 +1,12 @@
+create database ;
+use MBViajens;
+
+create table contato (
+id int auto_increment primary key,
+ir_de varchar(255) not null,
+ir_para varchar(255) not null,
+data_ida date,
+data_volta date,
+descrição varchar(255) not null,
+status ENUM('peding', 'completed') default 'peding'
+);
