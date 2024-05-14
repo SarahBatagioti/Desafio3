c3d37f5dcee760fc86ae6e8ea2296dd7eab74fde
Sarah Batagioti <sarah.montuanibt@gmail.com>
2024-05-14 08:35:08 -0300

    Conexão com banco e armazenamento de dados

--- app.py
+++ app.py
@@ -8,23 +8,8 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'fatec'
-app.config['MYSQL_DB'] = 'tasks'
+app.config['MYSQL_DB'] = 'MBViajens'
 mysql = MySQL(app)
-
-
-@app.route('/add', methods=['POST'])
-def add_contato():
-    if request.method == 'POST':
-        ir_de= request.form['ir_de']
-        ir_para = request.form['ir_para']
-        data_ida = request.form['data_ida']
-        data_volta = request.form['data_volta']
-        descrição = request.form['descrição']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO contato (ir_de, ir_para, data_ida, data_volta, descrição) VALUES (%s, %s)", (ir_de, ir_para, data_ida, data_volta, descrição))
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('index'))
 
 
 @app.route('/')
@@ -35,9 +20,29 @@ def index():
 def contatos():
     return render_template('contatos.html')
 
+
 @app.route('/serviços')
 def serviços():
-    return render_template('serviços.html')
+    #cur = mysql.connection.cursor()
+    #cur.execute("SELECT * FROM contato")
+    #contato = cur.fetchall()
+    #cur.close()
+    return render_template('serviços.html') #,contato=contato)
+
+# Adicionar contato
+@app.route('/add', methods=['POST'])
+def add_contato():
+    if request.method == 'POST':
+        ir_de= request.form['ir_de']
+        ir_para = request.form['ir_para']
+        data_ida = request.form['data_ida']
+        data_volta = request.form['data_volta']
+        descrição = request.form['descrição']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contato (ir_de, ir_para, data_ida, data_volta, descrição) VALUES (%s, %s, %s, %s, %s)", (ir_de, ir_para, data_ida, data_volta, descrição))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('contatos'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.sql
+++ script.sql
@@ -1,12 +1,11 @@
-create database ;
+create database MBViajens;
 use MBViajens;
 
 create table contato (
-id int auto_increment primary key,
-ir_de varchar(255) not null,
-ir_para varchar(255) not null,
-data_ida date,
-data_volta date,
-descrição varchar(255) not null,
-status ENUM('peding', 'completed') default 'peding'
+    id int auto_increment primary key,
+    ir_de varchar(255) not null,
+    ir_para varchar(255) not null,
+    data_ida date,
+    data_volta date,
+    descrição varchar(255) not null
 );
